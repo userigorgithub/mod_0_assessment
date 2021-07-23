fcae12e3b8808fe8107c9e5ba64ed2732605911e
Igor Decess <i.decess@comcast.net>
2021-07-23 15:27:31 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,17 @@
+var famousPerson = "Elton Jones";
+
+var isActive = false;
+
+famousPerson = "Michael Jackson";
+console.log(famousPerson);
+
+var num3 = 3;
+var num9 = 9;
+var sum = (num9 + num3);
+console.log(sum);
+
+var animals = ["Cow", "Pig", "Duck"];
+var count = animals.shift([0]);
+console.log(animals);
+
+famousPerson.length
