c7cf3760164d666fa2caa9c83a0b4eaf8f9c2807
Igor Decess <i.decess@comcast.net>
2021-07-23 15:46:23 -0600

    Add AIRPORT Object modelA.js

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var AIRPORT = {
+  name: "DIA",
+  country: "United States of America",
+  totalMaxAirplanes: 500,
+  totalMaxPeopleCap: 100000,
+  isBest: true,
+  isHaunted: true,
+  terminals: ["Concourse A", "Concourse B", "Concourse C"],
+  airlinesAvailable: ["Frontier", "Delta", "American", "United", "Southwest"]
+}
