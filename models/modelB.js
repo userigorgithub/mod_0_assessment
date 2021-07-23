0750f5ed66a14c59051bdd264fb2d11215b1e534
Igor Decess <i.decess@comcast.net>
2021-07-23 15:56:30 -0600

    Add myCurrentVehicle Info modelB.js

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var myCurrentVehicle = {
+  make: "Pontiac",
+  model: "Vibe",
+  peopleCapacity: 5,
+  odometer: 99000,
+  isActive: true,
+  inRepair: false,
+  drivers: ["Igor", "Tatsiana"],
+  statesDroveTo: ["Colorado", "Kansas"]
+}
